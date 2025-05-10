c0513e71d51d74fbaa66fcc591f3784f142580c7
Instelce <celestin.besson@proton.me>
2025-05-10 19:11:52 +0200

    some change

--- game/elements/components/heath_component.gd
+++ game/elements/components/heath_component.gd
@@ -2,10 +2,12 @@ class_name HealthComponent
 extends Node2D
 
 @export var max_health: float = 100.0
-var health: float
+@export var whant_health_bar: bool = true
 
 signal health_depleted
 signal health_changed(old_value, new_value)
+
+var health: float
 
 func _ready() -> void:
 	health = max_health
--- game/ui/ui_root.gd
+++ game/ui/ui_root.gd
@@ -3,6 +3,9 @@ extends CanvasLayer
 @onready var player: CharacterBody2D = %Player
 @onready var inventory_panel: InventoryDialog = %InventoryPanel
 
+
+func _process(delta: float) -> void:
+
 func _unhandled_input(event: InputEvent) -> void:
 	
 	# Open the inventory panel
